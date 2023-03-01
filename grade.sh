f04e64a71351348af9e55e98c69e37d615375299
zzzwww000 <120359926+zzzwww000@users.noreply.github.com>
2023-03-01 11:18:26 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,4 +1,4 @@
-CPATH='.:../lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
+CPATH='.:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar'
 
 # clone the repository of the student submission
 rm -rf submissions
