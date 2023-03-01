da1f324871aa5685c60b1c2b3afa9d3961cd9d4c
zzzwww000 <120359926+zzzwww000@users.noreply.github.com>
2023-03-01 02:20:38 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -1,10 +1,37 @@
 CPATH='.:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar'
 
-rm -rf student-submission
-git clone $1 student-submission
+# clone the repository of the student submission
+rm -rf submissions
+git clone $1 submissions
 echo 'Finished cloning'
 
-cp student-submission/ListExamples.java ./
-javac -cp $CPATH *.java
-java -cp $CPATH org.junit.runner.JUnitCore TestListExamples
+# check the student code has the correct file(s) submitted.
+if [ ! -f ./submissions/ListExamples.java ]; then
+    echo "You might have the wrong file submitted, please do check again!"
+    exit 1
+fi
 
+# Copy the student submission out of the submissions folder, and put it to the tests folder
+mkdir ./tests
+cp ./submissions/ListExamples.java ./tests
+cp ./TestListExamples.java ./tests
+
+# Compile the student's code and test files
+cd ./tests
+javac -cp $CPATH *.java
+
+# Check if compilation succeeded
+if [ $? -ne 0 ]; then
+    echo "Error: Compilation failed"
+    exit 1
+fi
+
+# Run the tests using JUnit and report the grade
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples | grep -E > tests_results.txt
+if [ $? -eq 0 ]; then
+    echo "Congratulations, you passed the tests!"
+else
+    echo "Sorry, you failed the tests."
+fi
+
+exit 0
